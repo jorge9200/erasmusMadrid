c0b72c80900495db33c9667acfc318157a6f8563
jorge9200 <jorge92f@gmail.com>
2015-04-28 23:22:18 +0200

    POST working
    He cambiado la query y he dejado todo preparado. Funciona y esta
    insertando usuarios sin problema.

--- routes/index.js
+++ routes/index.js
@@ -7,19 +7,17 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/registry', function(req, res, next){
-  var name_user=req.body.username;
-  var password=req.body.password;
-  var email=req.body.email;
-  //var date=req.body.date;
-  //if(req.param('saveForm')){
-    var id_user=1;
-    client.query('INSERT INTO user(id_user,name_user,password,email) VALUES(?,?,?,?)',[id_user,name_user,password,email],function(err,results,fields){
-      if(err){
-        throw err;
-      }
-    });
-    res.render('index', { title: 'Express' });
-    //console.dir(req.body);
+		
+		var name_user=req.body.username;
+		var password=req.body.password;
+		var email=req.body.email;
+		var id_user=2;
+	
+		insertUser(id_user, name_user, password, email, function(err, result){
+    	console.log(err || result);
+		});
+
+    console.dir(req.body);
 });
 
 module.exports = router;
