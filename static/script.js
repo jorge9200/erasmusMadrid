c97ec003c799388fbae4b40c4b88e36a07ca97ae
Renato Pinto <renapl90@gmail.com>
2015-05-12 17:35:29 +0200

    script.js y prueba del perfil

--- static/script.js
+++ static/script.js
@@ -1,6 +1,17 @@
 $( document ).ready(function() {
 
+	$("#load-header").load("header-footer.html #header");
+	$("#load-registry").load("header-footer.html #formRegistryModal");
+	$("#load-log").load("header-footer.html #formLogModal");
+	$("#load-footer").load("header-footer.html #footer");
 
-
+	$("#config").click(function(){
+		$("#configOptions").slideToggle(500);
+	});
+	$("#profileEvents").click(function(){
+		$("#ownEvents").slideToggle(500);
+	})
+	
 });
 
+	
