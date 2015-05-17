335eadf2372170b33305f088cfad21f728535191
ru10ben <rubenortizburgos@gmail.com>
2015-05-17 12:05:17 +0200

    Ya funciona.
    en listaEventos hay algo de prototype con los id's iguales y fallaba, ya
    se muestra el resultado de una consulta. Lo de prototype lo dejo
    comentado para que podais trabajar con ello descomentandolo.

--- static/listaEventos.js
+++ static/listaEventos.js
@@ -1,4 +1,11 @@
 $(function(){
+		// LOAD MODAL CABECERA Y FOOTER
+	$("#load-header").load("header-footer.html #header");
+	$("#load-registry").load("header-footer.html #formRegistryModal");
+	$("#load-log").load("header-footer.html #formLogModal");
+	$("#load-footer").load("header-footer.html #footer");
+	$("#load-createevent").load("header-footer.html #formCreateEvent")
+	
     $.get('/lista', function(data){
         $("#title").html(data[0]);
         $("#description").html(data[1]);
