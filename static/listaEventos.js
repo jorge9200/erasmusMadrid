060fb901b64807ff1bfac8c364dc51a62734d817
ru10ben <rubenortizburgos@gmail.com>
2015-05-21 10:43:09 +0200

    Intento fallido
    Esta comentado, estaba intentando mostrar también en infoDate los días
    restantes que quedan para el evento.

--- static/listaEventos.js
+++ static/listaEventos.js
@@ -8,6 +8,9 @@ $(function(){
     $.get('/lista', function(data){
         for (var i = 0; i < data.length; i++) {
             if(sel==data[i].category || sel=='Todos'){
+                //var prueba=(data[i].date).substring(11,19); Esta línea cogería la hora del dataTime de la BBDD
+                //var infoDate=restaFechas(data[i].date);
+                //console.log(data[i].date);
                 fechaOK=fechaCorrecta(data[i].date);
                 var description=(data[i].description).split('.');
                 description=description[0]+'...';
@@ -25,6 +28,7 @@ var addEvent = function(title, description,date){
     eventToDom.find('#title').text(title);
     eventToDom.find('#description').text(description);
     eventToDom.find('#date').text(date);
+    //eventToDom.find('#infoDate').text(infoDate);
 
     $('.search-menu').after(eventToDom);
     eventToDom.after('<hr>');
@@ -36,3 +40,21 @@ var fechaCorrecta = function(date){
     var fechaOK=fecha[2]+'/'+fecha[1]+'/'+fecha[0];
     return fechaOK;
 }
+
+// restaFechas = function(fecha1){
+// var aFecha2 = new Date().toJSON().slice(0,10)
+
+// aFecha2 = aFecha2.split('-');
+// console.log(aFecha1);
+
+// var aFecha1=fecha1.substring(0, 10);
+// aFecha1 = aFecha1.split('-');
+
+// console.log(aFecha1);
+
+// var fFecha1 = Date.UTC(aFecha1[2],aFecha1[1]-1,aFecha1[0]); 
+// var fFecha2 = Date.UTC(aFecha2[2],aFecha2[1]-1,aFecha2[0]); 
+// var dif = fFecha2 - fFecha1;
+// var dias = Math.floor(dif / (1000 * 60 * 60 * 24)); 
+// return dias;
+// }
