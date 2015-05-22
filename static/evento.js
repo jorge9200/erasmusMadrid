b890ea6be6b6ee269fc2c2a7ef373e3c9db2f934
Renato Pinto <renapl90@gmail.com>
2015-05-22 20:08:22 +0200

    localización en mapa
    Ya está puesto para que ponga una marca en la dirección del evento. Lo
    único que primero carga unas coordenadas fijas y después se va a la
    dirección correcta. He intentado ponerle que vaya directamente a la
    dirección correcta pero algo estaba liando que el mapa se quedaba gris
    xD ya lo seguiré mirando ;)

--- static/evento.js
+++ static/evento.js
@@ -1,33 +1,67 @@
 $(function(){
+/*Carga datos del evento*/
+	$.get('/evento', function(data){
+		fechaOK=fechaCorrecta(data[0].date);
+		horaOK=(data[0].date).substring(11, 16);
+		completeEvent(data[0].title,data[0].description,data[0].address,fechaOK,horaOK,data[0].comment);
+	});
+/***********************/
 
-    $.get('/evento', function(data){
-        fechaOK=fechaCorrecta(data[0].date);
-        horaOK=(data[0].date).substring(11, 16);
-        completeEvent(data[0].title,data[0].description,data[0].address,fechaOK,horaOK,data[0].comment);
-    });
+/*Carga el mapa con la direccion marcada*/
+	google.maps.event.addDomListener(window, 'load', cargaMapa);
+	//cargarDireccion();
+/****************************************/
 });
 
 
 var completeEvent = function(title,description,address,date,hour,comment){
-    /*Cargamos las imágenes*/
-    $('#eventImg1').attr("src", "./static/" + title + "1.jpg");
-    $('#eventImg2').attr("src", "./static/" + title + "2.jpg");
-    $('#eventImg3').attr("src", "./static/" + title + "3.jpg");
-    $('#eventImg4').attr("src", "./static/" + title + "4.jpg");
-    $('#eventImg5').attr("src", "./static/" + title + "5.jpg");
+	/*Cargamos las imágenes*/
+	$('#eventImg1').attr("src", "./static/" + title + "1.jpg");
+	$('#eventImg2').attr("src", "./static/" + title + "2.jpg");
+	$('#eventImg3').attr("src", "./static/" + title + "3.jpg");
+	$('#eventImg4').attr("src", "./static/" + title + "4.jpg");
+	$('#eventImg5').attr("src", "./static/" + title + "5.jpg");
 
-    /*Cargamos los campos de información*/
-    $('#eventTitle').text(title);
-    $('#eventDescription').text(description);
-    $('#eventAddress').text(address);
-    $('#eventDate').text(date);
-    $('#eventHour').text(hour);
-    $('#eventEslogan').text(comment);
+	/*Cargamos los campos de información*/
+	$('#eventTitle').text(title);
+	$('#eventDescription').text(description);
+	$('#eventAddress').text(address);
+	$('#eventDate').text(date);
+	$('#eventHour').text(hour);
+	$('#eventEslogan').text(comment);
 }
 
 var fechaCorrecta = function(date){
-    date=date.substring(0, 10);
-    var fecha=date.split('-');
-    var fechaOK=fecha[2]+'/'+fecha[1]+'/'+fecha[0];
-    return fechaOK;
+	date=date.substring(0, 10);
+	var fecha=date.split('-');
+	var fechaOK=fecha[2]+'/'+fecha[1]+'/'+fecha[0];
+	return fechaOK;
+}
+
+var cargaMapa = function initialize(){
+	var coord = new google.maps.LatLng(40.415152, -3.702341);
+	var geocoder = new google.maps.Geocoder();
+	var address = $('#eventAddress').text();
+	
+	var mapProp = {
+		center: coord,
+		zoom:17,
+		mapTypeId:google.maps.MapTypeId.ROADMAP
+	};
+
+	geocoder.geocode( { 'address': address}, function(results, status) {
+		if (status == google.maps.GeocoderStatus.OK) {
+			coord = results[0].geometry.location;
+			map.setCenter(coord);
+			var marker = new google.maps.Marker({
+				map: map,
+				position: coord
+			});
+		}
+		/*else {
+			alert('ERROR: ' + status);
+		}*/
+	});
+
+	var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
 }
