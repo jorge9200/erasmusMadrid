e1ad0563ab7b1484b1b0b434291767df04672eee
aminguito <a.minguito92@gmail.com>
2015-05-25 21:11:41 +0200

    Edición de perfil completada - cliente

--- static/perfil.js
+++ static/perfil.js
@@ -27,23 +27,99 @@ $(function(){
 
 /*******Animacion del menu*******/
 	//abre menu
+	var animated = false;
 	$('#config').click(function() {
-		$('.menu').animate({
-			left: '0px'
-		}, 200);
-		$('#wrapper').animate({
-			left: '285px'
-		}, 200);
+		if(animated == false) {
+			$('.menu').animate({
+				left: '0px'
+			}, 200);
+			$('#wrapper').animate({
+				left: '380px'
+			}, 200);
+			animated = true;
+		}else{
+			$('.menu').animate({
+				left: '-380px'
+			}, 200);
+			$('#wrapper').animate({
+				left: '0px'
+			}, 200);
+			animated = false;
+		}
 	});
 	
 	//cierra menu
 	$('.icon-close').click( function(){
 		$('.menu').animate({
-			left: '-285px'
+			left: '-380px'
 			}, 200);
 		$('#wrapper').animate({
 			left: '0px'
 		}, 200);
+	});
+	$('.foto-perfil').click(function(){
+		$('.perfil-guardar-foto').show();
+		$('#image-perfil').show();
+	});
+	$('.perfil-guardar-foto').click(function(){
+		if (checkValue($('#image-perfil').val(),'foto')) {
+			$.post( "/changeImage", $(this).serialize(), function( data ) {
+				$('#event-success').text("¡Enhorabuena! Ha modificado el parámetro correctamente");
+				$("#load-alert").load("header-footer.html #perfil-success");
+				window.setTimeout(function() { $("#perfil-success").alert('close'); }, 2000);
+			});
+		}
+	});
+	$('.perfil-guardar-usuario').click(function(){
+		if (checkValue($('#usuario-perfil').val(),'usuario')) {
+			$.post( "/changeUser", $(this).serialize(), function( data ) {
+				// Receive answer with OK (if the event isn't in the DB) or ERROR (if  is in the DB)
+				if (data == 'ERROR') {
+					$('.usuario-perfil-error .text').text("ERROR: El nombre de usuario ya existe");
+					$('.usuario-perfil-error').show();
+				}else{
+					$('.usuario-perfil-error .sr-only').text("");
+					$('.usuario-perfil-error').hide();
+					$('#event-success').text("¡Enhorabuena! Ha modificado el parámetro correctamente");
+					$("#load-alert").load("header-footer.html #perfil-success");
+					window.setTimeout(function() { $("#perfil-success").alert('close'); }, 2000);
+				}
+				});
+		}
+	});
+	$('.perfil-guardar-password').click(function(){
+		if (checkValue($('#password-perfil').val(),'password')) {
+			$.post( "/changePassword", $(this).serialize(), function( data ) {
+				$('#event-success').text("¡Enhorabuena! Ha modificado el parámetro correctamente");
+				$("#load-alert").load("header-footer.html #perfil-success");
+				window.setTimeout(function() { $("#perfil-success").alert('close'); }, 2000);
+			});
+		}
+	});
+	$('.perfil-guardar-mail').click(function(){
+		if (checkValue($('#mail-perfil').val(),'mail')) {
+			$.post( "/changeMail", $(this).serialize(), function( data ) {
+				$('#event-success').text("¡Enhorabuena! Ha modificado el parámetro correctamente");
+				$("#load-alert").load("header-footer.html #perfil-success");
+				window.setTimeout(function() { $("#perfil-success").alert('close'); }, 2000);
+			});
+		}
+	});
+	$('.usuario-perfil').click(function(){
+		$('.perfil-guardar-usuario').show();
+		$('#usuario-perfil').show();
+	});
+	$('.password-perfil').click(function(){
+		$('.perfil-guardar-password').show();
+		$('#password-perfil').show();
+	});
+	$('.email-perfil').click(function(){
+		$('.perfil-guardar-mail').show();
+		$('#mail-perfil').show();
+	});
+	// When focus out the email input
+	$('.mail-perfil').on('focusout',function(){
+		validateEmail($(this).val());
 	});
 /********************************/
 });
@@ -72,4 +148,30 @@ var fechaCorrecta = function(date){
 	var fecha=date.split('-');
 	var fechaOK=fecha[2]+'/'+fecha[1]+'/'+fecha[0];
 	return fechaOK;
+}
+
+// Validate/show/hide the email input and date error
+var validateEmail = function(email) {
+    var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+    if (!re.test(email)){
+    	$('.email-perfil-error .text').text("ERROR: Mail incorrecto");
+		$('.email-perfil-error').show();
+	}else{
+		$('.email-perfil-error .sr-only').text("");
+		$('.email-perfil-error').hide();
+    }
+}
+
+// Validate/show/hide the email input and date error
+var checkValue = function(param,value) {
+	var parametersOk = false;
+    if (param==''){
+    	$('.'+value+'-perfil-error .text').text("ERROR: Debe introducir "+value);
+		$('.'+value+'-perfil-error').show();
+	}else{
+		parametersOk = true;
+		$('.'+value+'-perfil-error .sr-only').text("");
+		$('.'+value+'-perfil-error').hide();
+    }
+    return parametersOk;
 }
